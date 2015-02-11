f4dad95e627c843335882fe20436b278fe75266e
vinay_vasista <vinay.vasista@csa.iisc.ernet.in>
2015-02-11 10:41:50 +0530

    moving gray inside bilateral pipeline computing gray in harris mode just for opencv rectangle for frame label

--- sandbox/video_demo/video_demo.py
+++ sandbox/video_demo/video_demo.py
@@ -5,7 +5,6 @@ import cv2
 import sys
 
 from common import clock, draw_str, StatValue
-#import video
 
 # load polymage shared libraries
 libharris = ctypes.cdll.LoadLibrary("./harris.so")
@@ -15,19 +14,16 @@ libbilateral = ctypes.cdll.LoadLibrary("./bilateral.so")
 harris = libharris.pipeline_harris
 unsharp = libunsharp.pipeline_mask
 bilateral = libbilateral.pipeline_bilateral
-bilateral_naive = libbilateral.pipeline_naive
 
 fn = sys.argv[1]
 cap = cv2.VideoCapture(fn)
 
 frames = 0
-#startTime = time.clock()
 startTime = time.clock()
 cv_mode = True
 harris_mode = False
 unsharp_mode = False
 bilateral_mode = False
-naive_mode = False
 
 thresh = 0.001
 weight = 3
@@ -39,14 +35,16 @@ while(cap.isOpened()):
     rows = frame.shape[0]
     cols = frame.shape[1]
     if harris_mode:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = np.float32(gray) / 4.0
         if cv_mode:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = np.float32(gray) / 4.0
             res = cv2.cornerHarris(gray, 3, 3, 0.04)
         else:
             res = np.zeros((rows, cols), np.float32) 
-            harris(ctypes.c_int(cols-2), ctypes.c_int(rows-2), \
-            ctypes.c_void_p(frame.ctypes.data), ctypes.c_void_p(res.ctypes.data))
+            harris(ctypes.c_int(cols-2), \
+                   ctypes.c_int(rows-2), \
+                   ctypes.c_void_p(frame.ctypes.data), \
+                   ctypes.c_void_p(res.ctypes.data))
         res = cv2.cvtColor(res, cv2.COLOR_GRAY2RGB)     
     elif unsharp_mode:
         res = np.empty((3, rows-4, cols-4), np.float32).ravel()
@@ -58,34 +56,34 @@ while(cap.isOpened()):
                 ctypes.c_void_p(res.ctypes.data))
         res = res.reshape(rows-4, cols-4, 3)
     elif bilateral_mode:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = np.float32(gray)
-        gray = gray/255
         res = np.zeros((rows, cols), np.float32)
-        if naive_mode:
-            bilateral_naive(ctypes.c_int(cols), ctypes.c_int(rows), \
-            ctypes.c_void_p(gray.ctypes.data), ctypes.c_void_p(res.ctypes.data))
-        else:
-            bilateral(ctypes.c_int(cols), ctypes.c_int(rows), \
-            ctypes.c_void_p(gray.ctypes.data), ctypes.c_void_p(res.ctypes.data))
+        bilateral(ctypes.c_int(cols), \
+                  ctypes.c_int(rows), \
+                  ctypes.c_void_p(frame.ctypes.data), \
+                  ctypes.c_void_p(res.ctypes.data))
     else:
         res = frame
 
     frameEnd = clock()
 
-    #cv2.rectangle(res, (0, 0), (750, 150), (255, 255, 255), thickness=cv2.cv.CV_FILLED)
+    cv2.rectangle(res, (0, 0), (750, 150), (255, 255, 255), thickness=cv2.cv.CV_FILLED)
 
-    draw_str(res, (40, 40), "frame interval :  %.1f ms" % (frameEnd*1000 - frameStart*1000))
+    draw_str(res, (40, 40),      "frame interval :  %.1f ms" % (frameEnd*1000 - frameStart*1000))
     if cv_mode and harris_mode:
-        draw_str(res, (40, 80), "Pipeline     :  " + str("OpenCV"))
+        draw_str(res, (40, 80),  "Pipeline        :  " + str("OpenCV"))
     elif bilateral_mode or harris_mode or unsharp_mode:
-        draw_str(res, (40, 80), "Pipeline     :  " + str("PolyMage"))
+        draw_str(res, (40, 80),  "Pipeline        :  " + str("PolyMage"))
+    else:
+        draw_str(res, (40, 80),  "Pipeline        :  ")
+
     if harris_mode:
-        draw_str(res, (40, 120), "Benchmark   :  " + str("Harris Corner"))
+        draw_str(res, (40, 120), "Benchmark    :  " + str("Harris Corner"))
     elif bilateral_mode:
-        draw_str(res, (40, 120), "Benchmark   :  " + str("Bilateral Grid"))
+        draw_str(res, (40, 120), "Benchmark    :  " + str("Bilateral Grid"))
     elif unsharp_mode:
-        draw_str(res, (40, 120), "Benchmark   :  " + str("Unsharp Mask"))
+        draw_str(res, (40, 120), "Benchmark    :  " + str("Unsharp Mask"))
+    else:
+        draw_str(res, (40, 120), "Benchmark    :  ")
 
     cv2.imshow('threaded video', res)
 
