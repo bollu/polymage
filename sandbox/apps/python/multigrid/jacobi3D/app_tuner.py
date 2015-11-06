e891b568a56fdc2b4f26290d156b75a75614fd7c
vinay_vasista <vinay.vasista@csa.iisc.ernet.in>
2015-11-06 14:54:05 +0530

    adding app tuner to jacobi-mg 3D

---
+++ sandbox/apps/python/multigrid/jacobi3D/app_tuner.py
@@ -0,0 +1,89 @@
+import sys
+
+from polymage_vcycle import vCycle
+from polymage_wcycle import wCycle
+from execMG import minimal_exec_mg
+from constructs import *
+
+from compiler import *
+import tuner
+
+def auto_tune(impipe_data, app_data):
+
+    cycle_type = app_data['cycle']
+    if cycle_type == 'V':
+        mg = vCycle(impipe_data, app_data)
+    elif cycle_type == 'W':
+        mg = wCycle(impipe_data, app_data)
+
+    app_name = app_data['cycle_name']
+    live_outs = [mg]
+    n = impipe_data['n']
+    param_estimates = [(n, app_data['n'])]
+    param_constraints = [ Condition(n, '==', app_data['n']) ]
+    dst_path = "/tmp"
+
+    group_size_configs = [3, 5, 7, 9, 11, 13, 15]
+
+    tile_size_configs = []
+    tile_size_configs.append([8, 8, 8])
+    tile_size_configs.append([8, 8, 16])
+    tile_size_configs.append([8, 8, 32])
+    tile_size_configs.append([8, 8, 64])
+    tile_size_configs.append([8, 16, 16])
+    tile_size_configs.append([8, 16, 32])
+    tile_size_configs.append([8, 16, 64])
+    tile_size_configs.append([8, 32, 32])
+    tile_size_configs.append([8, 32, 64])
+    tile_size_configs.append([8, 64, 64])
+
+    tile_size_configs.append([16, 16, 16])
+    tile_size_configs.append([16, 16, 32])
+    tile_size_configs.append([16, 16, 64])
+    tile_size_configs.append([16, 32, 32])
+    tile_size_configs.append([16, 32, 64])
+    tile_size_configs.append([16, 64, 64])
+
+    tile_size_configs.append([32, 32, 32])
+    tile_size_configs.append([32, 32, 64])
+    tile_size_configs.append([32, 64, 64])
+    tile_size_configs.append([64, 64, 64])
+
+    #opts = ['pool_alloc']
+    opts = []
+
+
+    # Generate Variants for Tuning
+    # ============================
+
+    gen_config = {"_tuner_app_name": app_name,
+                  "_tuner_live_outs": live_outs,
+                  "_tuner_param_constraints": param_constraints, #optional
+                  "_tuner_param_estimates": param_estimates, #optional
+                  "_tuner_tile_size_configs": tile_size_configs, #optional
+                  "_tuner_group_size_configs": group_size_configs, #optional
+                  "_tuner_opts": opts, #optional
+                  "_tuner_dst_path" : dst_path, # optional
+                  "_tuner_debug_flag": True, # optional
+                  "_tuner_opt_datadict": app_data
+                 }
+
+    _tuner_src_path, _tuner_configs_count, _tuner_pipe = \
+        tuner.generate(gen_config)
+
+
+    # Execute the generated variants
+    # ==============================
+
+    exec_config = {"_tuner_app_name": app_name,
+                   "_tuner_pipe": _tuner_pipe,
+                   "_tuner_src_path": _tuner_src_path, # optional
+                   "_tuner_configs_count": _tuner_configs_count, # optional
+                   "_tuner_omp_threads": 4, # optional
+                   "_tuner_nruns": 1, # optional
+                   "_tuner_debug_flag": True, # optional
+                   "_tuner_custom_executor": minimal_exec_mg,
+                   "_tuner_app_data": app_data
+                  }
+
+    tuner.execute(exec_config)
