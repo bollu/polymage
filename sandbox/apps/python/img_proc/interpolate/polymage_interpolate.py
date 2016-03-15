6bcb0c4b72443d83a3eb47e47a4eb8c943a1a8bf
vinay_vasista <vinay.vasista@csa.iisc.ernet.in>
2016-03-15 14:26:55 +0530

    multiscale interpolate : naming conventions

--- sandbox/apps/python/img_proc/interpolate/polymage_interpolate.py
+++ sandbox/apps/python/img_proc/interpolate/polymage_interpolate.py
@@ -39,107 +39,107 @@ def interpolate(pipe_data):
     ###########################################################################
 
     # DOWNSAMPLE
-    def pyrDown(f, l, name):
+    def pyr_down(f, l, name):
         # decrement factor
-        decFactor = 1 << l
+        dec_factor = 1 << l
         # original factor
-        orgFactor = 1 << (l-1)
+        org_factor = 1 << (l-1)
 
         # domain (downsampled)
-        decRowr = Interval(Int, 0, (R/decFactor)+1)
-        decColr = Interval(Int, 0, (C/decFactor)+1)
+        dec_rowr = Interval(Int, 0, (R/dec_factor)+1)
+        dec_colr = Interval(Int, 0, (C/dec_factor)+1)
         # domain (original)
-        colr = Interval(Int, 0, (C/orgFactor)+1)
+        colr = Interval(Int, 0, (C/org_factor)+1)
 
         # downsample in 'x' dimension (using [1 2 1]' filter)
 
         # body case
         condx = Condition(x, '>=', 1) & \
-                Condition(x, '<=', (R/decFactor)) & \
+                Condition(x, '<=', (R/dec_factor)) & \
                 Condition(y, '>=', 1) & \
-                Condition(y, '<=', (C/orgFactor))
+                Condition(y, '<=', (C/org_factor))
         # boundary cases
-        condxLeft   = Condition(y, '<=', 0)
-        condxRight  = Condition(y, '>=', (C/orgFactor)+1)
-        condxTop    = Condition(x, '<=', 0)
-        condxBottom = Condition(x, '>=', (R/decFactor)+1)
+        condx_left   = Condition(y, '<=', 0)
+        condx_right  = Condition(y, '>=', (C/org_factor)+1)
+        condx_top    = Condition(x, '<=', 0)
+        condx_bottom = Condition(x, '>=', (R/dec_factor)+1)
 
-        downx = Function(([c, x, y], [rgba, decRowr, colr]),
+        downx = Function(([c, x, y], [rgba, dec_rowr, colr]),
                          Float, "Dx_" + str(l) + "_" + name)
         downx.defn = [ Case(condx, (f(c, 2*x-2, y) + \
                                     f(c, 2*x-1, y) * 2.0 + \
                                     f(c, 2*x  , y) \
                                    ) * 0.25),
-                       Case(condxLeft, 0),
-                       Case(condxRight, 0),
-                       Case(condxBottom, 0),
-                       Case(condxTop, 0) ]
+                       Case(condx_left, 0),
+                       Case(condx_right, 0),
+                       Case(condx_bottom, 0),
+                       Case(condx_top, 0) ]
 
         # downsample in 'y' dimension (using [1 2 1] filter)
 
         condy = Condition(x, '>=', 1) & \
-                Condition(x, '<=', (R/decFactor)) & \
+                Condition(x, '<=', (R/dec_factor)) & \
                 Condition(y, '>=', 1) & \
-                Condition(y, '<=', (C/decFactor))
-        condyLeft   = Condition(y, '<=', 0)
-        condyRight  = Condition(y, '>=', (C/decFactor)+1)
-        condyTop    = Condition(x, '<=', 0)
-        condyBottom = Condition(x, '>=', (R/decFactor)+1)
+                Condition(y, '<=', (C/dec_factor))
+        condy_left   = Condition(y, '<=', 0)
+        condy_right  = Condition(y, '>=', (C/dec_factor)+1)
+        condy_top    = Condition(x, '<=', 0)
+        condy_bottom = Condition(x, '>=', (R/dec_factor)+1)
 
-        downy = Function(([c, x, y], [rgba, decRowr, decColr]),
+        downy = Function(([c, x, y], [rgba, dec_rowr, dec_colr]),
                          Float, "D_" + str(l) + "_" + name)
         downy.defn = [ Case(condy, (downx(c, x, 2*y-2) + \
                                     downx(c, x, 2*y-1) * 2.0 + \
                                     downx(c, x, 2*y  )
                                    ) * 0.25),
-                       Case(condyLeft, 0),
-                       Case(condyRight, 0),
-                       Case(condyBottom, 0),
-                       Case(condyTop, 0) ]
+                       Case(condy_left, 0),
+                       Case(condy_right, 0),
+                       Case(condy_bottom, 0),
+                       Case(condy_top, 0) ]
 
         return downy
 
     # UPSAMPLE & INTERPOLATE
-    def pyrUp_Xpolate(f, d, l, name):
-        decFactor = 1 << l+1
-        orgFactor = 1 << l
+    def pyr_up_Xpolate(f, d, l, name):
+        dec_factor = 1 << l+1
+        org_factor = 1 << l
 
         # domain (original)
-        decRowr = Interval(Int, 0, (R/decFactor)+1)
-        decColr = Interval(Int, 0, (C/decFactor)+1)
+        dec_rowr = Interval(Int, 0, (R/dec_factor)+1)
+        dec_colr = Interval(Int, 0, (C/dec_factor)+1)
 
         # domain (upsampled)
-        colr = Interval(Int, 0, (C/orgFactor)+1)
-        rowr = Interval(Int, 0, (R/orgFactor)+1)
+        colr = Interval(Int, 0, (C/org_factor)+1)
+        rowr = Interval(Int, 0, (R/org_factor)+1)
 
         # upsample in 'x' dimension
         condx = Condition(x, '>=', 1) & \
-                Condition(x, '<=', (R/orgFactor)) & \
+                Condition(x, '<=', (R/org_factor)) & \
                 Condition(y, '>=', 1) & \
-                Condition(y, '<=', (C/decFactor))
-        condxLeft   = Condition(y, '<=', 0)
-        condxRight  = Condition(y, '>=', (C/decFactor)+1)
-        condxTop    = Condition(x, '<=', 0)
-        condxBottom = Condition(x, '>=', (R/orgFactor)+1)
+                Condition(y, '<=', (C/dec_factor))
+        condx_left   = Condition(y, '<=', 0)
+        condx_right  = Condition(y, '>=', (C/dec_factor)+1)
+        condx_top    = Condition(x, '<=', 0)
+        condx_bottom = Condition(x, '>=', (R/org_factor)+1)
 
-        upx = Function(([c, x, y], [rgba, rowr, decColr]), Float, "Ux_" + name)
+        upx = Function(([c, x, y], [rgba, rowr, dec_colr]), Float, "Ux_" + name)
         upx.defn = [ Case(condx, (f(c, (x+1)/2, y) + \
                                   f(c, (x+2)/2, y) \
                                  ) / 2.0),
-                     Case(condxLeft, 0),
-                     Case(condxRight, 0),
-                     Case(condxBottom, 0),
-                     Case(condxTop, 0) ]
+                     Case(condx_left, 0),
+                     Case(condx_right, 0),
+                     Case(condx_bottom, 0),
+                     Case(condx_top, 0) ]
 
         # upsample in 'y' dimension and interpolate
         condy = Condition(x, '>=', 1) & \
-                Condition(x, '<=', (R/orgFactor)) & \
+                Condition(x, '<=', (R/org_factor)) & \
                 Condition(y, '>=', 1) & \
-                Condition(y, '<=', (C/orgFactor))
-        condyLeft   = Condition(y, '<=', 0)
-        condyRight  = Condition(y, '>=', (C/orgFactor)+1)
-        condyTop    = Condition(x, '<=', 0)
-        condyBottom = Condition(x, '>=', (R/orgFactor)+1)
+                Condition(y, '<=', (C/org_factor))
+        condy_left   = Condition(y, '<=', 0)
+        condy_right  = Condition(y, '>=', (C/org_factor)+1)
+        condy_top    = Condition(x, '<=', 0)
+        condy_bottom = Condition(x, '>=', (R/org_factor)+1)
 
         upy = (upx(c, x, (y+1)/2) + \
                upx(c, x, (y+2)/2)) / 2.0
@@ -147,10 +147,10 @@ def interpolate(pipe_data):
         interpolate = Function(([c, x, y], [rgba, rowr, colr]), Float, name)
         interpolate.defn = [ Case( condy, d(c, x, y) + \
                                           (1.0 - d(3, x, y)) * upy ),
-                             Case(condyLeft, 0),
-                             Case(condyRight, 0),
-                             Case(condyBottom, 0),
-                             Case(condyTop, 0) ]
+                             Case(condy_left, 0),
+                             Case(condy_right, 0),
+                             Case(condy_bottom, 0),
+                             Case(condy_top, 0) ]
 
         return interpolate
 
@@ -166,7 +166,7 @@ def interpolate(pipe_data):
     downsampled[0].defn = [img(c, x, y) * img(3, x, y)]
     # level : everything else
     for l in range(1, L):
-        downsampled[l] = pyrDown(downsampled[l-1], l, "downsampled")
+        downsampled[l] = pyr_down(downsampled[l-1], l, "downsampled")
 
     # 2.
     interpolated = {}
@@ -174,7 +174,7 @@ def interpolate(pipe_data):
     interpolated[L-1] = downsampled[L-1]
     # level : everything else
     for l in range(L-2, -1, -1):
-        interpolated[l] = pyrUp_Xpolate(interpolated[l+1], downsampled[l],
+        interpolated[l] = pyr_up_Xpolate(interpolated[l+1], downsampled[l],
                                         l, "interpolated_L" + str(l))
 
 
