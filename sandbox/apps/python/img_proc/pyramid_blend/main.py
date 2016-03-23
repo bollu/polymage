ad9a27667c3300c79890b23861d474d8fa462677
kumudhan <kumsbpips@gmail.com>
2016-03-23 10:23:15 +0530

    Adding tuner for Pyramid Blend benchmark

--- sandbox/apps/python/img_proc/pyramid_blend/main.py
+++ sandbox/apps/python/img_proc/pyramid_blend/main.py
@@ -8,7 +8,7 @@ from init import init_all
 from printer import print_header, print_config, print_line
 from builder import create_lib,build_pyramid
 from exec_pipe import pyramid_blending
-#from app_tuner import auto_tune
+from app_tuner import auto_tune
 
 app = "pyramid_blend"
 
@@ -22,7 +22,7 @@ def main():
     init_all(app_data)
     print_config(app_data)
     if app_data['mode'] == 'tune' or app_data['mode'] == 'tune_execute':
-        #auto_tune(pipe_data,app_data)
+        auto_tune(app_data)
         pass
     else:
         create_lib(build_pyramid, app, app_data)
