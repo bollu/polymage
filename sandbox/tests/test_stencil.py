b52d6585f94e1fb85296529e9aacdafd97acf318
vinay_vasista <vinay.vasista@csa.iisc.ernet.in>
2016-05-27 01:45:07 +0530

    add more rigorous test for Stencil construct

--- sandbox/tests/test_stencil.py
+++ sandbox/tests/test_stencil.py
@@ -8,40 +8,69 @@ sys.path.insert(0, '../')
 from compiler import *
 from constructs import *
 
+D = Parameter(Int, "D")
+R = Parameter(Int, "R")
+C = Parameter(Int, "C")
+z = Variable(Int, "z")
+y = Variable(Int, "y")
+x = Variable(Int, "x")
 
-def test_stencil():
-
-    R = Parameter(Int, "R")
-    C = Parameter(Int, "C")
-    x = Variable(Int, "x")
-    y = Variable(Int, "y")
+def test_stencil1():
 
     xrow = Interval(Int, 1, R)
     xcol = Interval(Int, 1, C)
 
     bounds = Condition(x, '>=', 2) & Condition(x, '<=', R - 2) & \
-        Condition(y, '>=', 2) & Condition(y, '<=', C - 2)
+             Condition(y, '>=', 2) & Condition(y, '<=', C - 2)
 
-    img = Image(Float, "input", [R + 1, C + 1])
+    img = Image(Float, "input", [R + 2, C + 2])
 
-    kernel = [[1, 0, 0], [0, 2, 0], [0, 0, 1]]
-    stencil = Stencil(img, [x, y], kernel)
-    print(stencil)
+    kernel1 = [[1, 0, 0], [0, 2, 0], [0, 0, 1]]
+    stencil_f1 = Stencil(img, [x, y], kernel1)
+    f1 = Function(([x, y], [xrow, xcol]), Float, "f1")
+    f1.defn = [Case(bounds, stencil_f1)]
 
-    f = Function(([x, y], [xrow, xcol]), Float, "stencilfn")
-    f.defn = [Case(bounds, stencil)]
+    kernel2 = [[-1, 2, -3.8], [0, -1, 0]]
+    stencil_f2 = Stencil(f1, [x, y], kernel2)
+    f2 = Function(([x, y], [xrow, xcol]), Float, "f2")
+    f2.defn = [Case(bounds, stencil_f2)]
 
-    groups = [f]
+    assert str(stencil_f1.input_func.name) == 'input'
+    assert stencil_f1.iter_vars == [x, y]
+    assert stencil_f1.sizes == [3, 3]
+    assert stencil_f1.origin == [1, 1]
+    assert stencil_f1.kernel == kernel1
+    assert str(Stencil.macro_expand(stencil_f1)) == \
+        '((input((x + -1), (y + -1)) + ' + \
+        '(input(x, y) * 2)) + ' + \
+        'input((x + 1), (y + 1)))'
+
+    assert str(stencil_f2.input_func.name) == 'f1'
+    assert stencil_f2.iter_vars == [x, y]
+    assert stencil_f2.sizes == [2, 3]
+    assert stencil_f2.origin == [0, 1]
+    assert stencil_f2.kernel == kernel2
+    assert str(Stencil.macro_expand(stencil_f2)) == \
+        '((((f1((x + -1), (y + -1)) * -1) + ' + \
+        '(f1((x + -1), y) * 2)) + ' + \
+        '(f1((x + -1), (y + 1)) * -3.8)) + ' + \
+        '(f1(x, y) * -1))'
+
+    kernel3 = [[[ 1,  2, -1], [-2,  1, -2], [-1,  2,  1]],
+               [[ 2,  1,  2], [ 1, -2,  1], [ 2,  1,  2]],
+               [[-1,  2,  1], [-2,  1, -2], [ 1,  2, -1]]]
+
+    groups = [f1, f2]
 
     p_est = [(R, 1024), (C, 1024)]
 
     # build the pipeline
-    pipeline = buildPipeline([f],
+    pipeline = buildPipeline([f2],
                              grouping=groups,
                              param_estimates=p_est,
-                             pipe_name="blur")
+                             pipe_name="stencil")
 
-    filename = "blur_graph"
+    filename = "stencil_graph"
     dot_file = filename + ".dot"
     png_file = filename + ".png"
     g = pipeline.pipeline_graph
@@ -54,7 +83,80 @@ def test_stencil():
     c_file.write(pipeline.generate_code().__str__())
     c_file.close()
 
+    return
 
-if __name__ == "__main__":
-    print("running test...")
-    test_stencil()
+def test_stencil2():
+
+    grid = Image(Float, "grid", [D + 2, R + 2, C + 2])
+
+    kernel1 = [[[ 1,  2, -1], [-2,  1, -2], [-1,  2,  1]],
+               [[ 2,  1,  2], [ 1, -2,  1], [ 2,  1,  2]],
+               [[-1,  2,  1], [-2,  1, -2], [ 1,  2, -1]]]
+    stencil1 = Stencil(grid, [x, y, z], kernel1)
+
+    assert stencil1.iter_vars == [x, y, z]
+    assert stencil1.sizes == [3, 3, 3]
+    assert stencil1.origin == [1, 1, 1]
+    assert stencil1.kernel == kernel1
+    assert str(Stencil.macro_expand(stencil1)) == \
+        '(((((((((((((((((((((((((' + \
+        '(grid((x + -1), (y + -1), (z + -1)) + ' + \
+        '(grid((x + -1), (y + -1), z) * 2)) + ' + \
+        '(grid((x + -1), (y + -1), (z + 1)) * -1)) + ' + \
+        '(grid((x + -1), y, (z + -1)) * -2)) + ' + \
+        'grid((x + -1), y, z)) + ' + \
+        '(grid((x + -1), y, (z + 1)) * -2)) + ' + \
+        '(grid((x + -1), (y + 1), (z + -1)) * -1)) + ' + \
+        '(grid((x + -1), (y + 1), z) * 2)) + ' + \
+        'grid((x + -1), (y + 1), (z + 1))) + ' + \
+        '(grid(x, (y + -1), (z + -1)) * 2)) + ' + \
+        'grid(x, (y + -1), z)) + ' + \
+        '(grid(x, (y + -1), (z + 1)) * 2)) + ' + \
+        'grid(x, y, (z + -1))) + ' + \
+        '(grid(x, y, z) * -2)) + ' + \
+        'grid(x, y, (z + 1))) + ' + \
+        '(grid(x, (y + 1), (z + -1)) * 2)) + ' + \
+        'grid(x, (y + 1), z)) + ' + \
+        '(grid(x, (y + 1), (z + 1)) * 2)) + ' + \
+        '(grid((x + 1), (y + -1), (z + -1)) * -1)) + ' + \
+        '(grid((x + 1), (y + -1), z) * 2)) + ' + \
+        'grid((x + 1), (y + -1), (z + 1))) + ' + \
+        '(grid((x + 1), y, (z + -1)) * -2)) + ' + \
+        'grid((x + 1), y, z)) + ' + \
+        '(grid((x + 1), y, (z + 1)) * -2)) + ' + \
+        'grid((x + 1), (y + 1), (z + -1))) + ' + \
+        '(grid((x + 1), (y + 1), z) * 2)) + ' + \
+        '(grid((x + 1), (y + 1), (z + 1)) * -1))'
+
+
+    kernel2 = [[[ 1,  2], [-2,  1], [-1,  2]],
+               [[ 2,  1], [ 1, -2], [ 2,  1]]]
+    stencil2 = Stencil(grid, [x, y, z], kernel2)
+
+    assert stencil2.iter_vars == [x, y, z]
+    assert stencil2.sizes == [2, 3, 2]
+    assert stencil2.origin == [0, 1, 0]
+    assert stencil2.kernel == kernel2
+    assert str(Stencil.macro_expand(stencil2)) == \
+        '((((((((((' + \
+        '(grid((x + -1), (y + -1), (z + -1)) + ' + \
+        '(grid((x + -1), (y + -1), z) * 2)) + ' + \
+        '(grid((x + -1), y, (z + -1)) * -2)) + ' + \
+        'grid((x + -1), y, z)) + ' + \
+        '(grid((x + -1), (y + 1), (z + -1)) * -1)) + ' + \
+        '(grid((x + -1), (y + 1), z) * 2)) + ' + \
+        '(grid(x, (y + -1), (z + -1)) * 2)) + ' + \
+        'grid(x, (y + -1), z)) + ' + \
+        'grid(x, y, (z + -1))) + ' + \
+        '(grid(x, y, z) * -2)) + ' + \
+        '(grid(x, (y + 1), (z + -1)) * 2)) + ' + \
+        'grid(x, (y + 1), z))'
+
+    return
+
+def _test_stencil3():
+    kernel = [[[ 1,  2], [-2,  1, -2], [-1,  2,  1]],
+              [[ 2,  1], [ 1, -2,  1], [ 2,  1,  2]]]
+    assert is_valid_kernel(kernel, 3) == False
+
+    return
