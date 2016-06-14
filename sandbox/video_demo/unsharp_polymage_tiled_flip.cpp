85966b7f5f9d8a0aa0f2649e873ce6833c76776b
Krishna Kinnal <krishnakinnal@gmail.com>
2016-06-14 09:25:06 +0000

    Loop bound initialization for inner loops pushed to the outer loops

--- sandbox/video_demo/unsharp_polymage_tiled_flip.cpp
+++ sandbox/video_demo/unsharp_polymage_tiled_flip.cpp
@@ -13,15 +13,6 @@ extern "C" void  pipeline_mask(int  C, int  R, float  threshold, float  weight, 
   float * mask_flip;
   mask_flip = (float *) (mask_void_arg);
 
-    //float * img = (float *)malloc(sizeof(float) * 3 * (R+4) * (C+4));
-/*
-    #pragma omp parallel for schedule(static)
-    for(int i = 0; i < R+4; i++)
-        for(int j = 0; j< C+4; j++)
-            for(int c = 0; c < 3; c++)
-                img[c*(R+4)*(C+4) + i*(C+4) + j] = ((float)img_flip[i*(C+4)*3 + j*3 + c]) / 255.0f;
-*/
-
   #pragma omp parallel for schedule(static)
   for (int  _T_i1 = 0; (_T_i1 <= ((R + 1) / 32)); _T_i1 = (_T_i1 + 1))
   {
@@ -29,16 +20,28 @@ extern "C" void  pipeline_mask(int  C, int  R, float  threshold, float  weight, 
     float  blurx[3][32][262];
     float  blury[3][32][262];
     float  sharpen[3][32][262];
+    int  _ct0 = (((R + 1) < ((32 * _T_i1) + 31)) ? (R + 1): ((32 * _T_i1) + 31));
+    int  _ct1 = ((2 > (32 * _T_i1))? 2: (32 * _T_i1));
+    int  _ct4 = (((R + 1) < ((32 * _T_i1) + 31))? (R + 1): ((32 * _T_i1) + 31));
+    int  _ct5 = ((2 > (32 * _T_i1))? 2: (32 * _T_i1));
+    int  _ct8 = (((R + 1) < ((32 * _T_i1) + 31))? (R + 1): ((32 * _T_i1) + 31));
+    int  _ct9 = ((2 > (32 * _T_i1))? 2: (32 * _T_i1));
+    int  _ct12 = (((R + 1) < ((32 * _T_i1) + 31))? (R + 1): ((32 * _T_i1) + 31));
+    int  _ct13 = ((2 > (32 * _T_i1))? 2: (32 * _T_i1));
     for (int  _T_i2 = -1; (_T_i2 <= ((C + 3) / 256)); _T_i2 = (_T_i2 + 1))
     {
+      int  _ct2 = (((C + 3) < ((256 * _T_i2) + 261))? (C + 3): ((256 * _T_i2) + 261));
+      int  _ct3 = ((0 > (256 * _T_i2))? 0: (256 * _T_i2));
+      int  _ct6 = (((C + 1) < ((256 * _T_i2) + 260))? (C + 1): ((256 * _T_i2) + 260));
+      int  _ct7 = ((2 > ((256 * _T_i2) + 1))? 2: ((256 * _T_i2) + 1));
+      int  _ct10 = (((C + 1) < ((256 * _T_i2) + 259))? (C + 1): ((256 * _T_i2) + 259));
+      int  _ct11 = ((2 > ((256 * _T_i2) + 2))? 2: ((256 * _T_i2) + 2));
+      int  _ct14 = (((C + 1) < ((256 * _T_i2) + 258))? (C + 1): ((256 * _T_i2) + 258));
+      int  _ct15 = ((2 > ((256 * _T_i2) + 3))? 2: ((256 * _T_i2) + 3));
       for (int  _i0 = 0; (_i0 <= 2); _i0 = (_i0 + 1))
-      {
-        int  _ct0 = (((R + 1) < ((32 * _T_i1) + 31)) ? (R + 1): ((32 * _T_i1) + 31));
-        int  _ct1 = ((2 > (32 * _T_i1))? 2: (32 * _T_i1));
+      {     
         for (int  _i1 = _ct1-2; (_i1 <= _ct0+2); _i1 = (_i1 + 1))
         {
-          int  _ct2 = (((C + 3) < ((256 * _T_i2) + 261))? (C + 3): ((256 * _T_i2) + 261));
-          int  _ct3 = ((0 > (256 * _T_i2))? 0: (256 * _T_i2));
           #pragma ivdep
           for (int  _i2 = _ct3; (_i2 <= _ct2); _i2 = (_i2 + 1))
           {
@@ -48,24 +51,17 @@ extern "C" void  pipeline_mask(int  C, int  R, float  threshold, float  weight, 
 
         for (int  _i1 = _ct1; (_i1 <= _ct0); _i1 = (_i1 + 1))
         {
-          int  _ct2 = (((C + 3) < ((256 * _T_i2) + 261))? (C + 3): ((256 * _T_i2) + 261));
-          int  _ct3 = ((0 > (256 * _T_i2))? 0: (256 * _T_i2));
           #pragma ivdep
           for (int  _i2 = _ct3; (_i2 <= _ct2); _i2 = (_i2 + 1))
           {
             blurx[_i0][((-32 * _T_i1) + _i1)][((-256 * _T_i2) + _i2)] = img[_i0][(-32 * _T_i1)-2 + _i1][(-256 * _T_i2)+_i2] * 0.0625f + img[_i0][(-32 * _T_i1)-1 + _i1][(-256 * _T_i2)+_i2] * 0.25f + img[_i0][(-32 * _T_i1)+_i1][(-256 * _T_i2)+_i2] * 0.375f + img[_i0][(-32 * _T_i1)+1 + _i1][(-256 * _T_i2)+_i2] * 0.25f + img[_i0][(-32 * _T_i1)+2 + _i1][(-256 * _T_i2)+_i2] * 0.0625f;
-            //blurx[_i0][((-32 * _T_i1) + _i1)][((-256 * _T_i2) + _i2)] = (((((img[(((_i0 * ((R + 4) * (C + 4))) + ((-2 + _i1) * (C + 4))) + _i2)] * 0.0625f) + (img[(((_i0 * ((R + 4) * (C + 4))) + ((-1 + _i1) * (C + 4))) + _i2)] * 0.25f)) + (img[(((_i0 * ((R + 4) * (C + 4))) + (_i1 * (C + 4))) + _i2)] * 0.375f)) + (img[(((_i0 * ((R + 4) * (C + 4))) + ((1 + _i1) * (C + 4))) + _i2)] * 0.25f)) + (img[(((_i0 * ((R + 4) * (C + 4))) + ((2 + _i1) * (C + 4))) + _i2)] * 0.0625f));
           }
         }
       }
       for (int  _i0 = 0; (_i0 <= 2); _i0 = (_i0 + 1))
       {
-        int  _ct4 = (((R + 1) < ((32 * _T_i1) + 31))? (R + 1): ((32 * _T_i1) + 31));
-        int  _ct5 = ((2 > (32 * _T_i1))? 2: (32 * _T_i1));
         for (int  _i1 = _ct5; (_i1 <= _ct4); _i1 = (_i1 + 1))
         {
-          int  _ct6 = (((C + 1) < ((256 * _T_i2) + 260))? (C + 1): ((256 * _T_i2) + 260));
-          int  _ct7 = ((2 > ((256 * _T_i2) + 1))? 2: ((256 * _T_i2) + 1));
           #pragma ivdep
           for (int  _i2 = _ct7; (_i2 <= _ct6); _i2 = (_i2 + 1))
           {
@@ -75,12 +71,8 @@ extern "C" void  pipeline_mask(int  C, int  R, float  threshold, float  weight, 
       }
       for (int  _i0 = 0; (_i0 <= 2); _i0 = (_i0 + 1))
       {
-        int  _ct8 = (((R + 1) < ((32 * _T_i1) + 31))? (R + 1): ((32 * _T_i1) + 31));
-        int  _ct9 = ((2 > (32 * _T_i1))? 2: (32 * _T_i1));
         for (int  _i1 = _ct9; (_i1 <= _ct8); _i1 = (_i1 + 1))
         {
-          int  _ct10 = (((C + 1) < ((256 * _T_i2) + 259))? (C + 1): ((256 * _T_i2) + 259));
-          int  _ct11 = ((2 > ((256 * _T_i2) + 2))? 2: ((256 * _T_i2) + 2));
           #pragma ivdep
           for (int  _i2 = _ct11; (_i2 <= _ct10); _i2 = (_i2 + 1))
           {
@@ -90,12 +82,8 @@ extern "C" void  pipeline_mask(int  C, int  R, float  threshold, float  weight, 
       }
       for (int  _i0 = 0; (_i0 <= 2); _i0 = (_i0 + 1))
       {
-        int  _ct12 = (((R + 1) < ((32 * _T_i1) + 31))? (R + 1): ((32 * _T_i1) + 31));
-        int  _ct13 = ((2 > (32 * _T_i1))? 2: (32 * _T_i1));
         for (int  _i1 = _ct13; (_i1 <= _ct12); _i1 = (_i1 + 1))
         {
-          int  _ct14 = (((C + 1) < ((256 * _T_i2) + 258))? (C + 1): ((256 * _T_i2) + 258));
-          int  _ct15 = ((2 > ((256 * _T_i2) + 3))? 2: ((256 * _T_i2) + 3));
           #pragma ivdep
           for (int  _i2 = _ct15; (_i2 <= _ct14); _i2 = (_i2 + 1))
           {
